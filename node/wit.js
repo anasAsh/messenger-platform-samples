3ec1df365e0d84c3bc16669afda484d874a2313b
Anas Alhamdan <anas@bluekangaroo.co>
2017-08-05 00:46:06 +0300

    added with NLP

---
+++ node/wit.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+
+module.exports = (text) => {
+  const options = {
+    url: 'https://api.wit.ai/message?v=20170804&q='+encodeURI(text),
+    headers: {
+      'Authorization': 'Bearer R3KXVHFW3TEN5HRLAQUGISWCGHHGZTHQ'
+    }
+  };
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+         var witResponse = JSON.parse(body);
+         console.log('witResponse', witResponse);
+         resolve(witResponse);
+      }
+      reject(error);
+    });
+  });
+
+};
